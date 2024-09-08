b0085868c3e00dfe7217a3fda7c9e3b3a765050a
Soya Sano <soyasano2180plus1@gmail.com>
2024-09-08 17:12:43 +0900

    add youtube player for trailer

--- src/components/Row/Layout.tsx
+++ src/components/Row/Layout.tsx
@@ -1,13 +1,32 @@
+import YouTube from "react-youtube";
 import { Movie } from "../../type";
 
 type LayoutProps = {
     title: string;
     isLargeRow?: boolean;
     movies: Movie[];
+    trailerUrl: string | null;
+    handleClick: (movie: Movie) => void;
 };
 
-export const Layout = ({ title, isLargeRow, movies }: LayoutProps) => { 
+// youtubeのiframeトレーラー動画の設定値
+type Options = {
+    height: string;
+    width: string;
+    playerVars: {
+        autoplay: 0 | 1 | undefined;
+    };
+}
+
+export const Layout = ({ title, isLargeRow, movies, trailerUrl, handleClick }: LayoutProps) => { 
     const image_url = "https://image.tmdb.org/t/p/original";
+    const opts: Options = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
     return (
         <div className="ml-5 text-white">
             <h2>{title}</h2>
@@ -18,11 +37,13 @@ export const Layout = ({ title, isLargeRow, movies }: LayoutProps) => {
                         className={`object-contain w-full max-h-24 m-2 transform transition-transform duration-450 ${isLargeRow ? "max-h-60 hover:scale-110" : "hover:scale-108"}`}
                         src={`${image_url}${
                             isLargeRow ? movie.poster_path : movie.backdrop_path
-                        }`}
+                            }`}
+                        onClick={() => handleClick(movie)}
                         alt={movie.name}
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 };
--- src/components/Row/useProps.ts
+++ src/components/Row/useProps.ts
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import { Movie } from "../../type";
+import { requests } from "../../request";
 
 export const useProps = (fetchUrl: string) => { 
     const [movies, setMovies] = useState<Movie[]>([]);
+
+    const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
 
     // 非同期で映画データを取得
     useEffect(() => {
@@ -22,5 +25,18 @@ export const useProps = (fetchUrl: string) => {
         fetchData();
     }, [fetchUrl]);
 
-    return { movies };
+    const handleClick = async (movie: Movie) => { 
+        if (trailerUrl) {
+            setTrailerUrl("null");
+        } else {
+            const moviePlayUrl = await axios.get(requests.fetchMovieVideos(Number(movie.id)));
+            setTrailerUrl(moviePlayUrl.data.results[0]?.key);
+        }
+    };
+
+    return {
+        movies,
+        trailerUrl,
+        handleClick,
+    };
 }
